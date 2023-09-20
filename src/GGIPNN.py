f3fdb619430d87a4589ff1b02a0a1b2a8d354ea9
lerdeljan17 <laxy982@gmail.com>
2023-09-20 07:22:24 +0000

    updated GGIPNN, added forward

--- src/GGIPNN.py
+++ src/GGIPNN.py
@@ -1,54 +1,78 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
+import numpy as np
 
+# Define the GGIPNN model
 class GGIPNN(nn.Module):
-    """
-    A neural network for text classification.
-    Uses an embedding layer, followed by a hidden and softmax layer.
-    """
-    def __init__(
-      self, sequence_length, num_classes, vocab_size,
-      embedding_size, hidden_dimension=100, embedTrain = False, l2_lambda =0):
-
+    def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, hidden_dimension=100, embedTrain=False, l2_lambda=0):
         super(GGIPNN, self).__init__()
-
-        # Placeholders for input, output and dropout
-        self.input_x = torch.nn.Variable(torch.long, [None, sequence_length], name="input_x")
-        self.input_y = torch.nn.Variable(torch.float, [None, num_classes], name="input_y")
-        self.dropout_keep_prob = torch.nn.Variable(torch.float, name="dropout_keep_prob")
 
         # Embedding layer
         self.embedding = nn.Embedding(vocab_size, embedding_size)
-        if embedTrain:
-            self.embedding.weight.requires_grad = True
+        if not embedTrain:
+            self.embedding.weight.requires_grad = False
 
-        # Hidden layer
-        self.W2 = nn.Linear(embedding_size * sequence_length, hidden_dimension)
-        self.b2 = nn.Parameter(torch.ones([hidden_dimension]))
+        # Hidden layers
+        self.fc1 = nn.Linear(embedding_size * sequence_length, hidden_dimension)
+        self.fc2 = nn.Linear(hidden_dimension, 100)
+        self.fc3 = nn.Linear(100, num_classes)
 
-        self.W3 = nn.Linear(hidden_dimension, hidden_dimension)
-        self.b3 = nn.Parameter(torch.ones([hidden_dimension]))
+        # Dropout layer
+        self.dropout = nn.Dropout(p=0.5)
 
-        self.W4 = nn.Linear(hidden_dimension, 10)
-        self.b4 = nn.Parameter(torch.ones([10]))
+    def forward(self, x):
+        # Embedding layer
+        embedded_chars = self.embedding(x)
 
-        # Dropout
-        self.dropout = nn.Dropout(1 - self.dropout_keep_prob)
+        # Flatten the embeddings
+        embedded_chars_flat = embedded_chars.view(embedded_chars.size(0), -1)
 
-        # Final (unnormalized) scores and predictions
-        self.scores = self.W4(F.relu(self.W3(F.relu(self.dropout(self.W2(self.embedding(self.input_x))))))) + self.b4
-        self.softmax_scores = F.softmax(self.scores, dim=1)
-        self.predictions = torch.argmax(self.scores, 1)
+        # Hidden layers with ReLU activation and dropout
+        hidden1 = F.relu(self.fc1(embedded_chars_flat))
+        hidden2 = F.relu(self.fc2(hidden1))
+        hidden2_drop = self.dropout(hidden2)
 
-        # CalculateMean cross-entropy loss
-        self.loss = F.cross_entropy(self.scores, self.input_y)
-        l2_losses = torch.tensor([0.0]).to(self.loss.device)
-        for param in self.parameters():
-            if param.requires_grad:
-                l2_losses += torch.norm(param) * l2_lambda
-        self.loss += l2_losses
+        # Output layer
+        logits = self.fc3(hidden2_drop)
 
-        # Accuracy
-        self.accuracy = torch.mean(torch.eq(self.predictions, self.input_y))
+        # Softmax and predictions
+        softmax_scores = F.softmax(logits, dim=1)
+        predictions = torch.argmax(logits, dim=1)
 
+        return logits, softmax_scores, predictions
+
+# Sample training loop
+def train(model, x_train, y_train, batch_size, num_epochs):
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(num_epochs):
+        model.train()
+        for i in range(0, len(x_train), batch_size):
+            x_batch = x_train[i:i + batch_size]
+            y_batch = y_train[i:i + batch_size]
+
+            optimizer.zero_grad()
+            logits, _, _ = model(x_batch)
+            loss = criterion(logits, torch.argmax(y_batch, dim=1))
+            loss.backward()
+            optimizer.step()
+
+            if (i + 1) % 100 == 0:
+                print(f"Epoch {epoch + 1}/{num_epochs}, Batch {i + 1}/{len(x_train)}, Loss: {loss.item()}")
+
+# Example usage
+if __name__ == "__main__":
+    # Sample data
+    sequence_length = 50
+    num_classes = 10
+    vocab_size = 10000
+    embedding_size = 200
+    x_train = torch.randint(0, vocab_size, (1000, sequence_length))
+    y_train = torch.randint(0, num_classes, (1000,))
+
+    # Create and train the model
+    model = GGIPNN(sequence_length, num_classes, vocab_size, embedding_size)
+    train(model, x_train, y_train, batch_size=64, num_epochs=5)
